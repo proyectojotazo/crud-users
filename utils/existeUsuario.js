bc4c1b7a90fd15150da491e8cdfe8e58a25b744a
Javier <javigh87@gmail.com>
2021-08-30 09:36:43 +0200

    Refactorizado y documentado

--- utils/existeUsuario.js
+++ utils/existeUsuario.js
@@ -1,30 +1,32 @@
-const chalk = require('chalk')
+const { underlineRed, green } = require('./colores')
 
 const compruebaUsuario = (user_name = '', pass = '', listaUsuarios = []) => {
-    const usuarioEncontrado = existeUsuario(user_name, listaUsuarios)
-    if (usuarioEncontrado.length === 0){
-        console.log()
-        console.log(chalk.red('El usuario y/o la contraseña son incorrectas'))
-      } else {
-        if(passCorrecto(pass, usuarioEncontrado)){
-          console.log()
-          console.log(chalk.green(`Bienvenido a la app ${user_name}`))
-        } else {
-          console.log()
-          console.log(chalk.red('El usuario y/o la contraseña son incorrectas'))
-        }
-      }
-    return usuarioEncontrado
+  const usuarioEncontrado = existeUsuario(user_name, listaUsuarios)
+  if (usuarioEncontrado.length === 0) { // Si no se encuentra el usuario
+    console.log()
+    console.log(underlineRed('El usuario y/o la contraseña son incorrectas'))
+  } else { // Si se encuentra el usuario
+    if (passCorrecto(pass, usuarioEncontrado)) { // Si el password coincide
+      console.log()
+      console.log(green(`Bienvenido a la app ${user_name}`))
+    } else { // Si el password no coincide
+      console.log()
+      console.log(underlineRed('El usuario y/o la contraseña son incorrectas'))
+    }
+  }
+  return usuarioEncontrado // Será un objeto con todos los parametros o []
 }
 
-const existeUsuario = ( user_name = '', listaUsuarios = [] ) => {
-    return listaUsuarios.filter(usuario => usuario.nombre_usuario === user_name)[0] || []
+const existeUsuario = (user_name = '', listaUsuarios = []) => {
+  // Comprobamos si existe el usuario en la lista de usuarios
+  return listaUsuarios.filter(usuario => usuario.nombre_usuario === user_name)[0] || []
 }
 
-const passCorrecto = ( pass = '', usuarioEncontrado = {} ) => {
-    return pass === usuarioEncontrado.pass
+const passCorrecto = (pass = '', usuarioEncontrado = {}) => {
+  // Comprobamos que el password del usuario coincida con el introducido
+  return pass === usuarioEncontrado.pass
 }
 
 module.exports = {
-    compruebaUsuario,
+  compruebaUsuario,
 }
