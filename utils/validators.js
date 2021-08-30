7542c7306bee602d6aae682e36ff1c4cbc4f2c93
Javier <javigh87@gmail.com>
2021-08-30 09:37:16 +0200

    Refactorizado y documentado

--- utils/validators.js
+++ utils/validators.js
@@ -1,52 +1,51 @@
 const { underlineRed, white, green } = require('./colores')
-// TODO: Crear en chalk todos los colores relacionados a éste archivo
 
-const validaNombreApellido = (value = '') => {
-
-    const regex = /^[a-zA-Z]{3,}([\s]{1}[a-zA-Z]{1,}){0,}?$/i
-
-    const errorMsg = 
-    ` ${underlineRed('El campo no cumple con alguno de estos criterios:')}
+const msgValidators = {
+    nombreApellidos: ` ${underlineRed('El campo no cumple con alguno de estos criterios:')}
     ${white('- No puede estar vacío')}
     ${white('- Debe contener un mínimo de 3 carácteres')}
     ${white('- No puede contener espacios adicionales')}
     ${white('- No acepta carácteres especiales. Ej: ')}${green('+*_-')}    
-    `
-
-    return regex.test(value) || errorMsg
-}
-
-const validaUsuario = (value = '') => {
-    // TODO: Crear una regex
-    if (value.length === 0) {
-        return 'No se puede dejar el campo vacío'
-    } else if (value.length > 18) {
-        return 'El usuario no puede contener más de 18 carácteres'
-    }
-    
-    return true
-}
-
-const validaEmail = (value = '') => {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    
-    const errorMsg = `${underlineRed('Debe de ser un email válido')}`
-    
-    return regex.test(value) || errorMsg
-}
-
-const validaPassword = (value = '') => {
-    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
-    
-    const errorMsg = 
-    ` ${underlineRed('El campo no cumple con alguno de estos criterios:')}
+    `,
+    usuario: ` ${underlineRed('El campo no cumple con alguno de estos criterios:')}
+    ${white('- No puede estar vacío')}
+    ${white('- Debe contener un mínimo de 3 carácteres')}
+    ${white('- No puede contener espacios adicionales')}
+    ${white('- Solo acepta guiones y guión bajo como caracter especial')}    
+    `,
+    email: `${underlineRed('Debe de ser un email válido')}`,
+    password: ` ${underlineRed('El campo no cumple con alguno de estos criterios:')}
     ${white('- Debe contener un mínimo de 8 carácteres')}
     ${white('- Debe contener una mayúscula')}
     ${white('- Debe contener una minúscula')}
     ${white('- Debe contener un dígito')}    
     `
 
-    return regex.test(value) || errorMsg
+}
+
+const validaNombreApellido = (value = '') => {
+    const regex = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+([\s]{1}[a-zA-ZÀ-ÿ\u00f1\u00d1]{2,})?$/
+
+    return regex.test(value) || msgValidators.nombreApellidos
+}
+
+const validaUsuario = (value = '') => {
+
+    const regex = /^[a-zA-Z0-9À-ÿ\u00f1\u00d1_-]{3,16}$/
+       
+    return regex.test(value) || msgValidators.usuario
+}
+
+const validaEmail = (value = '') => {
+    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    
+    return regex.test(value) || msgValidators.email
+}
+
+const validaPassword = (value = '') => {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+    
+    return regex.test(value) || msgValidators.password
 }
 
 module.exports = {
